582195a19484158d3aee745124a39131e76be068
Joyonta Saha <joyontasaha8@gmail.com>
2021-09-10 12:21:51 +0600

    initial commit

---
+++ src/test/java/com/joyonta/springsecuritywithdatabase/SpringSecurityWithDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyonta.springsecuritywithdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
