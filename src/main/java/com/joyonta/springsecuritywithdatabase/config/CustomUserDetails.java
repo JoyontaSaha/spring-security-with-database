e467fd4313e5ff422de29c49c2b5edda49a3bf55
Joyonta Saha <joyontasaha8@gmail.com>
2021-09-10 13:16:00 +0600

    created custom user datails and custom user details service classes and configured

---
+++ src/main/java/com/joyonta/springsecuritywithdatabase/config/CustomUserDetails.java
@@ -0,0 +1,60 @@
+package com.joyonta.springsecuritywithdatabase.config;
+
+import com.joyonta.springsecuritywithdatabase.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetails implements UserDetails {
+
+    @Autowired
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    /**
+     * this implementation is only for user having single role
+     * for user having multiple roles custom implement accordingly
+     *
+     */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(user.getRole()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
